e2a81f8e53379c2066759781a802fc18cbc4d249
aiksu <xah1111@gmail.com>
2018-08-17 14:58:20 +0300

    domain added

--- core/forms.py
+++ core/forms.py
@@ -1,13 +1,17 @@
 # from django.forms import forms
 from django import forms
 
+def _parsePhoneNumber(tel_number):
+    return tel_number.replace('(', '').replace(')', '').replace('_', '').replace('-', '').replace(' ', '')
 
 class CustomSignupForm(forms.Form):
     first_name = forms.CharField(max_length=30, label='Имя')
     last_name = forms.CharField(max_length=30, label='Фамилия')
+    phone = forms.CharField(max_length=30, label='Телефон')
 
     def signup(self, request, user):
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
+        user.username = _parsePhoneNumber(self.cleaned_data['phone'])
 
         user.save()
