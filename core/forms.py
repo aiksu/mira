faca78388e09fecf059a3616bfee274f50d7760d
aiksu <xah1111@gmail.com>
2018-09-10 11:00:07 +0300

    changes 23

--- core/forms.py
+++ core/forms.py
@@ -2,7 +2,13 @@
 from django import forms
 
 def _parsePhoneNumber(tel_number):
-    return tel_number.replace('(', '').replace(')', '').replace('_', '').replace('-', '').replace(' ', '')
+    return tel_number\
+        .replace('(', '')\
+        .replace(')', '')\
+        .replace('_', '')\
+        .replace('-', '')\
+        .replace(' ', '')\
+        .replace('+','')
 
 class CustomSignupForm(forms.Form):
     first_name = forms.CharField(max_length=30, label='Имя')
@@ -12,6 +18,6 @@ class CustomSignupForm(forms.Form):
     def signup(self, request, user):
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
-        # user.username = _parsePhoneNumber(self.cleaned_data['phone'])
+        user.username = _parsePhoneNumber(self.cleaned_data['phone'])
 
         user.save()
